50b5db45e6cb2cca93c5ea4b95f94514c35977bb
Santiago Quenguan Ordoñez <santiagoquen2007@gmail.com>
2026-03-24 08:35:09 -0500

    codigo mejorado

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App {
         
            biblioteca.Biblioteca biblioteca = new Biblioteca("Central");
 
-           System.out.println("==============Biblioteca Central==================");
+           System.out.println("==============BIBLIOTECA CENTRAL==================");
 
           
            
